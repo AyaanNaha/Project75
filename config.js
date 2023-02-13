101ea21d6eee8423795cbeb546802f1c88f07f7b
AyaanNaha <ayaan.naha26@gmail.com>
2023-02-13 12:46:39 -0800

    finished project

--- config.js
+++ config.js
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import firebase from "firebase";
+require("@firebase/firestore");
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBKJkTzJPh-T_79XLGWdxagAeQR2nNpsZQ",
   authDomain: "e-ride-a53ea.firebaseapp.com",
@@ -13,5 +10,6 @@ const firebaseConfig = {
   appId: "1:808284902790:web:41c60dc4d5d94739044807"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
